06b91c1971ac490bf54a93aa1352cb7aced9f897
enricosada <enrico@sada.io>
2014-03-18 17:58:15 +0100

    add fsc unit tests

---
+++ src/fsharp/FSharp.Compiler.Unittests/Driver/file_version_info.fs
@@ -0,0 +1,15 @@
+namespace FSharp.Compiler.Unittests.Driver
+
+open System
+open System.Numerics 
+open FSharp.Core.Unittests.LibraryTestFx
+open NUnit.Framework
+
+open Microsoft.FSharp.Compiler.Driver
+
+[<TestFixture>]
+type ``File Version info`` () =
+
+    [<Test>] member x.``Get File info`` () =
+        //Assert.AreEqual(y, y |> LanguagePrimitives.SByteWithMeasure<m> |> sbyte)
+        ()
